1b94f70c5019f782d3eecda9aeaa2737919ae6cf
InoueShinichi <phinesze@gmail.com>
2023-03-11 12:44:15 +0900

    nuxtjs/tailwindcssを導入する

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        '@nuxt/content'
+        '@nuxt/content',
+        '@nuxtjs/tailwindcss',
     ],
 })
