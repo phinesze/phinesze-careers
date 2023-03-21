199a05d079ccfcc03616e475aa5ed44921a1cb22
InoueShinichi <phinesze@gmail.com>
2023-03-21 14:17:01 +0900

    EnvironmentElement.vue, 15.tsをformat

--- components/EnvironmentElement.vue
+++ components/EnvironmentElement.vue
@@ -1,27 +1,24 @@
 <script setup lang="ts">
-import {careers} from "~/constants/careers";
+import { careers } from "~/constants/careers";
 
 const props = defineProps<{
-  element: string | [string, { version?: number | string }?]
-}>()
+  element: string | [string, { version?: number | string }?];
+}>();
 
 const elementName = computed(() => {
-  return typeof props.element === 'string' ? props.element : props.element[0];
-})
+  return typeof props.element === "string" ? props.element : props.element[0];
+});
 
 const elementOptions = computed(() => {
-  return typeof props.element === 'string' ? undefined : (props.element[1]);
-})
-
+  return typeof props.element === "string" ? undefined : props.element[1];
+});
 </script>
 
 <template>
   <div class="inline mr-2">
     <span>{{ elementName }}</span>
     <template v-if="elementOptions">
-      <i v-if="elementOptions.version">
-          v{{ elementOptions.version }}
-      </i>
+      <i v-if="elementOptions.version"> v{{ elementOptions.version }} </i>
     </template>
   </div>
 </template>
