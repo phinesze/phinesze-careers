9985ba23ba34fd83ea7cdace88e7f808a2c4e60f
InoueShinichi <phinesze@gmail.com>
2023-03-21 18:40:01 +0900

    KeirekiDetailTableのカラー設定をcolからtdに移動させる

--- components/KeirekiDetailTable.vue
+++ components/KeirekiDetailTable.vue
@@ -5,17 +5,17 @@ import { careers } from "~/constants/careers";
 <template>
   <table class="table-fixed">
     <colgroup>
-      <col class="w-[8mm] bg-gray-400" />
-      <col class="w-0.5 w-[8mm] bg-lime-300" />
+      <col class="w-[8mm]" />
+      <col class="w-0.5 w-[8mm]" />
       <col class="w-[21mm]" />
       <col class="w-[auto]" />
       <col class="w-[50mm]" />
     </colgroup>
     <template v-for="company in careers">
       <template v-for="career in company.careers" :key="career.id">
-        <tr>
-          <td rowspan="2">{{ company.company }}</td>
-          <td rowspan="2">#{{ career.id }}</td>
+        <tr class="career-row break-before-auto">
+          <td class="bg-gray-400" rowspan="2">{{ company.company }}</td>
+          <td class="bg-lime-300" rowspan="2">#{{ career.id }}</td>
           <th class="p-2">期間</th>
           <td class="font-bold p-2 bg-cyan-100">{{ career.title }}</td>
           <th class="p-2">言語・フレームワーク</th>
