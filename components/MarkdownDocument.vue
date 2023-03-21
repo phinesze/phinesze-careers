2899093a530a10ebb07e863da06683f8af05e11d
InoueShinichi <phinesze@gmail.com>
2023-03-21 16:41:14 +0900

    markdownに独自にスタイルを設定させる目的のため、MarkdownDocument.vueを作成

---
+++ components/MarkdownDocument.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+  path: string;
+}>();
+</script>
+
+<template>
+  <section>
+    <ContentDoc :path="path" />
+  </section>
+</template>
+
+<style scoped lang="scss">
+:deep {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    @apply font-bold;
+  }
+  li {
+    @apply list-inside list-disc;
+  }
+}
+</style>
