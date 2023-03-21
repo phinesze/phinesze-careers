c271cde37d02a769d453dad75f816fa73c0f9e7e
InoueShinichi <phinesze@gmail.com>
2023-03-21 14:18:48 +0900

    careers.tsファイルの内容をKeirekiDetailTable.vueで表示できるようにする

--- components/KeirekiDetailTable.vue
+++ components/KeirekiDetailTable.vue
@@ -1,29 +1,45 @@
+<script setup lang="ts">
+import { careers } from "~/constants/careers";
+</script>
+
 <template>
   <table>
-    <tr style="">
-      <td rowspan="2">K社</td>
-      <td rowspan="2">#21</td>
-      <th class="p-2">期間</th>
-      <td class="p-2">データ活用のマーケティングツールの開発</td>
-      <th class="p-2">言語・フレームワーク</th>
-    </tr>
-    <tr>
-      <td>(期間詳細)</td>
-      <td>
-        システム概要
-        ファーストパーティーデータ活用のデータ統合ツールの開発を行う。 担当業務
-        ・フロントエンドの改修依頼を行う。 （休日メインの稼働）
-      </td>
-      <td>
-        ・フロントエンド React, Next.js、Tailwind css、Jest、 ・バックエンド
-        Laravel、MongoDB ・その他 docker
-      </td>
-    </tr>
+    <template v-for="company in careers">
+      <template v-for="career in company.careers">
+        <tr style="">
+          <td rowspan="2">{{ company.company }}</td>
+          <td rowspan="2">#{{ career.id }}</td>
+          <th class="p-2">期間</th>
+          <td class="p-2">{{ career.title }}</td>
+          <th class="p-2">言語・フレームワーク</th>
+        </tr>
+        <tr>
+          <td>
+            <template v-if="career.times">
+              <div v-if="career.times.start">{{ career.times.start }} ~</div>
+              <div v-if="career.times.end">
+                {{ career.times.end }}
+              </div>
+            </template>
+          </td>
+          <td>
+            システム概要
+          </td>
+          <td>
+            <ul>
+              <li v-for="(elements, name) in career.environments">
+                <p class="font-bold">{{ name }}</p>
+                <div>
+                  <EnvironmentElement
+                    v-for="element in elements"
+                    :element="element"
+                  />
+                </div>
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </template>
+    </template>
   </table>
 </template>
-
-<script lang="ts">
-export default {
-  name: "KeirekiDetailTable",
-};
-</script>
