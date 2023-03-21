29425ace6a8b7359600550002b01af44e45a3ac2
InoueShinichi <phinesze@gmail.com>
2023-03-21 17:41:38 +0900

    「期間」部の表示をフォーマットさせるためのIntervalDate.vueを作成する

---
+++ components/IntervalDate.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+  value?: string;
+}>();
+
+const dateString = computed(() => {
+  if (props.value) {
+    const [year, month, date] = props.value.split("-");
+    return `${Number(year)}年${Number(month)}月`;
+  } else {
+    return null;
+  }
+});
+</script>
+
+<template>
+  <span>
+    {{ dateString }}
+  </span>
+</template>
