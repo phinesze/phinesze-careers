06254ea9a04a01098d6f6d9e30e2b7878530f8a5
InoueShinichi <phinesze@gmail.com>
2023-03-21 14:15:30 +0900

    開発環境の一つ一つのコンポーネントであるEnvironmentElement.vueを追加

---
+++ components/EnvironmentElement.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {careers} from "~/constants/careers";
+
+const props = defineProps<{
+  element: string | [string, { version?: number | string }?]
+}>()
+
+const elementName = computed(() => {
+  return typeof props.element === 'string' ? props.element : props.element[0];
+})
+
+const elementOptions = computed(() => {
+  return typeof props.element === 'string' ? undefined : (props.element[1]);
+})
+
+</script>
+
+<template>
+  <div class="inline mr-2">
+    <span>{{ elementName }}</span>
+    <template v-if="elementOptions">
+      <i v-if="elementOptions.version">
+          v{{ elementOptions.version }}
+      </i>
+    </template>
+  </div>
+</template>
