054ed2c42d92f043720d15d442dd3a79b0b07e95
InoueShinichi <phinesze@gmail.com>
2023-03-21 14:21:11 +0900

    EnvironmentElement.vue内にtype EnvironmentElementを定義する

--- components/EnvironmentElement.vue
+++ components/EnvironmentElement.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { careers } from "~/constants/careers";
 
+type EnvironmentElement = string | [string, { version?: number | string }?];
+
 const props = defineProps<{
-  element: string | [string, { version?: number | string }?];
+  element: EnvironmentElement;
 }>();
 
 const elementName = computed(() => {
