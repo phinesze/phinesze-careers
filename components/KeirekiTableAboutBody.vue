daddf82683c3a7fc1e47d41342d27451521e0e22
InoueShinichi <phinesze@gmail.com>
2023-03-29 02:12:26 +0900

    テーブルで上寄せしたいテーブルの箇所にalign-topを追加

--- components/KeirekiTableAboutBody.vue
+++ components/KeirekiTableAboutBody.vue
@@ -2,7 +2,7 @@
   <tbody>
   <tr>
     <th class="p-4" colspan="2">自己PR</th>
-    <td class="p-4" colspan="3">
+    <td class="p-4 align-top" colspan="3">
       <MarkdownDocument path="/about" />
     </td>
   </tr>
--- components/KeirekiTableSupplementBody.vue
+++ components/KeirekiTableSupplementBody.vue
@@ -2,7 +2,7 @@
   <tbody>
     <tr>
       <th class="p-4" colspan="2" rowspan="20">補足</th>
-      <td class="p-4" colspan="3" rowspan="20">
+      <td class="p-4 align-top" colspan="3" rowspan="20">
         <MarkdownDocument path="/supplement" />
       </td>
     </tr>
