8a062975c9b7927c760b00973e13c57930254182
InoueShinichi <phinesze@gmail.com>
2023-03-28 21:54:18 +0900

    経歴テーブルの自己PR部分をKeirekiTableAboutBodyに分離

--- components/KeirekiTable.vue
+++ components/KeirekiTable.vue
@@ -10,14 +10,7 @@
       <col class="w-[auto]" />
       <col class="w-[50mm]" />
     </colgroup>
-    <tbody>
-      <tr>
-        <th class="p-4" colspan="2">自己PR</th>
-        <td class="p-4" colspan="3">
-          <MarkdownDocument path="/about" />
-        </td>
-      </tr>
-    </tbody>
+    <KeirekiTableAboutBody />
     <KeirekiTableCareersBody />
     <KeirekiTableSupplementBody />
   </table>
