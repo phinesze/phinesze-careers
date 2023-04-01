664a561ad31a1d7eef78443a485427cba4cd4a94
InoueShinichi <phinesze@gmail.com>
2023-04-01 16:52:16 +0900

    atomic designに対応したnuxt.config.tsの設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  components: {
+    dirs: [
+      "~/components/atoms",
+      "~/components/molecules",
+      "~/components/organisms",
+      "~/components",
+    ],
+  },
 });
