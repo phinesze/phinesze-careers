10a9c3092bd9155bd7bb472f6f0041ef17a4796b
InoueShinichi <phinesze@gmail.com>
2023-04-02 19:42:41 +0900

    職務経歴書のCaptionに更新日時を設定させる

--- components/organisms/CareerTable/index.vue
+++ components/organisms/CareerTable/index.vue
@@ -1,7 +1,10 @@
 <template>
   <table class="table-fixed target-table">
-    <caption class="text-5xl mb-5">
-      職務経歴書
+    <caption class="relative mb-5">
+      <div class="text-5xl">職務経歴書</div>
+      <div class="absolute right-0 bottom-0 text-sm">
+        <DateLabel value="2023-04-02" /> 更新
+      </div>
     </caption>
     <colgroup>
       <col class="w-[8mm]" />
@@ -10,10 +13,16 @@
       <col class="w-[auto]" />
       <col class="w-[50mm]" />
     </colgroup>
-    <CareerTableDocumentBody label="自己PR" markdownPath="/about" />
-    <CareerTableDocumentBody label="主な経験" markdownPath="/frameworks" />
-    <CareerTableDocumentBody label="アカウント" markdownPath="/accounts" />
+    <CareerTableDocumentBody label="自己PR" markdown-path="/about" />
+    <CareerTableDocumentBody label="主な経験" markdown-path="/frameworks" />
+    <CareerTableDocumentBody label="アカウント" markdown-path="/accounts" />
     <CareerTableProjectsBody />
-    <CareerTableDocumentBody label="一問一答" markdownPath="/supplement" />
+    <CareerTableDocumentBody label="一問一答" markdown-path="/supplement" />
   </table>
 </template>
+<script>
+import DateLabel from "../../atoms/DateLabel";
+export default {
+  components: { DateLabel },
+};
+</script>
