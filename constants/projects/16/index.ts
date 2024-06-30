14b2bc5da20840182fc6b92eef8ae2d595d9ec9a
InoueShinichi <phinesze@gmail.com>
2024-06-30 11:40:29 +0900

    16,19,20,22を更新

--- constants/projects/16/index.ts
+++ constants/projects/16/index.ts
@@ -1,9 +1,26 @@
 import { Project } from "~/types/Project";
-import { detail } from "~/constants/projects/16/detail";
+
+export const detail = `
+#### 【システム概要】
+
+ファッションECサイトのフロントエンドをメインとした開発やリリース作業をする。
+
+#### 【担当フェーズ】
+
+開発・保守運用
+
+#### 【担当業務】
+
+WrikeのチケットやAdobeXDの内容に従いフロントエンド側の改修作業や新規コンポーネントを開発しました。
+
+#### 【備考】
+- React部分の改修に携わる可能性があったが、予算の都合上の関係で終了
+
+`;
 
 export const project16: Project = {
   id: 16,
-  title: "既存ファッションECサイトの開発",
+  title: "ファッションアイテムを中心としたECサイトの改修業務",
   detail,
   times: {
     start: "2021-07",
