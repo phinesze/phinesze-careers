73bb723f2ac9f347bcc8065c0f34d4dbfb8e8ff6
InoueShinichi <phinesze@gmail.com>
2024-07-09 21:04:16 +0900

    about.tsを更新

--- constants/doduments/about.ts
+++ constants/doduments/about.ts
@@ -1,7 +1,7 @@
 export const about = `
-私はJavaScript（TypeScript）・Vue.js・React・Angularなどを採用したフロントエンドと、Node.js・PHP・Javaなどを使用したバックエンドでのWeb開発をメインとしてまいりました。
+私はJavaScript（TypeScript）・Vue.js・React・Angularなどのフロントエンドや、Node.js・PHP・JavaなどのバックエンドのフレームワークでのWeb開発をメインとしてまいりました。
 学生時代にはJava・C++・C#等のプログラミング言語を学んでおり・IT業界に就業後は、
-Linux（CentOS6〜7・Ubuntu18）・PHP（5・7）・MySQL・HTML5（CSS3・JavaScriptを含む）・Vue.js・React等のフロントエンドフレームワークやDockerのようなコンテナシステム等のWeb関連の技術を習得してまいりました。
+Linux（CentOS6〜7・Ubuntu18）・PHP（5・7）・MySQL・PostgreSQLなどのSQL・Vue.js・React等のフロントエンドフレームワークやDockerのようなコンテナシステム等のWeb関連の技術を習得してまいりました。
 
 現在ではnpm・yarn・Vue.js（Composition API）・Nuxt.jsを中心としたフロントエンドでの業務を中心に開発しておりますが・
 Node.js（NestJS）やPHP等のバックエンド部分・bash・zsh等のコマンドラインシェルの操作や・MySQL・PostgreSQL等のDBの使用・Google Cloud・Firebaseなど含めて業務・個人開発を含めWebに関する幅広い経験がございます。
