98b227a385502b718010302f360fe7ce9ba8a7c7
inoueshinichi <phinesze@gmail.com>
2025-08-07 10:13:42 +0900

    Tableだった時のDocumentBodyをTableDocumentBodyとして追加

---
+++ components/organisms/CareerTable/TableDocumentBody.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  markdownText?: string;
+}>();
+</script>
+
+<template>
+  <tbody>
+    <tr>
+      <!-- A,B,C 本文 -->
+      <td class="p-0 align-top" colspan="3">
+        <div v-if="label" class="font-bold bg-gray-300 p-2">
+          {{ label }}
+        </div>
+        <MarkdownDocument
+          class="p-4"
+          :label="label"
+          :markdown-text="markdownText"
+        />
+      </td>
+    </tr>
+  </tbody>
+</template>
